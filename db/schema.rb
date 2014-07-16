f57ed959bce36f32c921dd635e1462e6a490d963
Aleksey Gureiev <spyromus@noizeramp.com>
2014-07-17 00:21:47 +0400

    added password init feature and activity logging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716112251) do
+ActiveRecord::Schema.define(version: 20140716202128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +64,15 @@ ActiveRecord::Schema.define(version: 20140716112251) do
     t.datetime "reset_password_email_sent_at"
     t.string   "phone"
     t.string   "title"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.boolean  "password_set",                    default: false, null: false
+    t.string   "last_login_from_ip_address"
   end
 
   add_index "users", ["account_id"], name: "index_users_on_account_id", using: :btree
+  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
